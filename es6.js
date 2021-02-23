ae142b6d8bb288f56a64c19aaecfe8bccc7396ff
aditya <adityafernanda2013@gmail.com>
2021-02-23 23:24:09 +0700

    es6 dasar javascript

---
+++ es6.js
@@ -0,0 +1,28 @@
+// let + cons
+    // Normal javascript
+    var x = 1;
+
+    if ( x === 1) {
+        var x = 2;
+        console.log(x); //expected output: 2
+    }
+    console.log(x); //2
+
+    // ES6
+    let y = 1;
+
+    if (y === 1) {
+        let y = 2;
+
+        console.log(y)
+    }
+
+    console.log(y)
+    const number = 42;
+    number = 100;
+
+
+// Arrow Function
+    //Normal javascript
+    
+
